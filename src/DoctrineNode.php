aac599d25ef26310a61600a3098e2d1a2879e453
James Kirkby <me@jameskirkby.com>
2016-10-14 20:24:05 +0100

    updates

--- src/DoctrineNode.php
+++ src/DoctrineNode.php
@@ -9,7 +9,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
  * Class Node
  *
  * @package app\Entities
- * @author James Kirkby <me@jameskirkby.com>
+ * @author James Kirkby <jkirkby91@gmail.com>
  *
  * @ORM\MappedSuperclass
  */
